fc5b9797214055ba9d951aa912658effd0813d13
Adrian Lopez <adrijardi@gmail.com>
2014-11-03 16:04:03 +0000

    another test

--- opsworks_initial_setup/attributes/customize.rb
+++ opsworks_initial_setup/attributes/customize.rb
@@ -1,2 +1,2 @@
-normal[:opsworks_initial_setup][:sysctl]['net.ipv4.tcp_keepalive_time'] = 512
-normal['opsworks_initial_setup']['swapfile_size_mb'] = 2048
+normal[:opsworks_initial_setup][:sysctl]['net.ipv4.tcp_keepalive_time'] = 1024
+normal[:opsworks_initial_setup]['swapfile_size_mb'] = 2048
