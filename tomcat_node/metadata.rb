de60b174f5bab35859d7dc7806b272840a156cc0
Adrian Lopez <adrijardi@gmail.com>
2014-11-04 14:46:41 +0000

    nat

---
+++ tomcat_node/metadata.rb
@@ -0,0 +1,7 @@
+name             'tomcat_node'
+maintainer       'Adrian Lopez'
+maintainer_email 'adrijardi@gmail.com'
+license          'All rights reserved'
+description      'Configures an standard tomcat node'
+version          '1.0.0'
+depends          "simple_iptables"
